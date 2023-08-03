1c1f95454d5d13b26c94e08626b8a126af8a2ba6
SheldonYoungo <sheldomyoungo@gmail.com>
2023-08-03 13:20:47 -0400

    Fixed bug

--- projects/03-tic-tac-toe/src/components/Board.jsx
+++ projects/03-tic-tac-toe/src/components/Board.jsx
@@ -2,16 +2,16 @@ import { Square } from "./Square"
 
 export function Board ({board, updateBoard}) {
     return (
-            board.map((square, index) => {
-              return (
-                <Square
-                  key={index}
-                  index={index}
-                  updateBoard={updateBoard}
-                >
-                  {square}
-                </Square>
-              )
-            })
+      board.map((square, index) => {
+        return (
+          <Square
+            key={index}
+            index={index}
+            updateBoard={updateBoard}
+          >
+            {square}
+          </Square>
+        )
+      })
     )
 }
