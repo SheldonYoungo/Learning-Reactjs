8ad6ccb4a272f2f0671fa71a62568ec9ceca2434
SheldonYoungo <sheldomyoungo@gmail.com>
2023-08-10 13:14:39 -0400

    Tested button changes

--- projects/05-prueba-tecnica-junior-trainee/tests/example.spec.js
+++ projects/05-prueba-tecnica-junior-trainee/tests/example.spec.js
@@ -6,7 +6,7 @@ const LOCALHOST_URL = 'http://localhost:5173/'
 
 test('app shows random fact and image', async ({ page }) => {
   await page.goto(LOCALHOST_URL);
-
+  
   const text = await page.getByRole('paragraph')
   const image = await page.getByRole('img')
 
@@ -16,3 +16,19 @@ test('app shows random fact and image', async ({ page }) => {
   await expect(textContent?.length).toBeGreaterThan(0)
   await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
 });
+
+test('checks if the fact and the image changes when the button is clicked', async ({page}) => {
+  await page.goto(LOCALHOST_URL)
+
+  const firstFact = await page.getByRole('paragraph').textContent()
+  const firstImage = await page.getByRole('img').getAttribute('src')
+
+  await page.getByRole('button').click()
+  await page.waitForTimeout(1000)
+
+  const secondFact = await page.getByRole('paragraph').textContent()
+  const secondImage = await page.getByRole('img').getAttribute('src')
+
+  expect(secondFact, 'should be a different fact').not.toEqual(firstFact)
+  expect(secondImage, 'should be a different image').not.toEqual(firstImage)
+})
